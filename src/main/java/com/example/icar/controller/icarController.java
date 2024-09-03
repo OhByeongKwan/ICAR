82eb3a5f420a5f7f5a8e53a29c0759870a75920d
오병관 <obk9184@naver.com>
2024-09-03 19:47:57 +0900

    start project

---
+++ src/main/java/com/example/icar/controller/icarController.java
@@ -0,0 +1,12 @@
+package com.example.icar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class icarController {
+    @GetMapping("/")
+    public String main(){
+        return "/index";
+    }
+}
---
+++ src/test/java/com/example/icar/IcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
